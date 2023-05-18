3ce0ca9eafede079d34a4a1eaf9a2bf61ab9fba7
Yan Marques <65124745+yvnmarquxs@users.noreply.github.com>
2023-05-18 15:59:39 -0300

    Create 2164.c

---
+++ 2164.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    double n, x, y, ans;
+    scanf("%lf", &n);
+    x = (1 + sqrt(5))/2.0;
+    y = (1 - sqrt(5))/2.0;
+    ans = (pow(x,n) - pow(y,n))/sqrt(5);
+    printf("%.1lf\n", ans);
+    return 0;
+}
